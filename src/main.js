159a028b5d3eef4764230cb93813cdd4f79d32bc
ericwang1902 <ericwang1902@163.com>
2016-09-26 23:18:20 +0800

    添加vue-router组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+
 Vue.use(Element)
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
